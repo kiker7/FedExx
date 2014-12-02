6256c3cd2d92ffa797867949f16c9b3bfda7c9d8
kiker7 <rafalrutyna00@gmail.com>
2014-12-02 15:11:32 +0100

    odczyt zlecenia
    wstepnie

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5f/9038195b1c7a001415daab1800cb37f1
@@ -0,0 +1,11 @@
+package logic;
+
+public class Order {
+
+	private int id;
+	private int baseCity;
+	private int orderCity;
+	private String name;
+	private int priority;
+	
+}
--- SDP/src/logic/Order.java
+++ SDP/src/logic/Order.java
@@ -1,6 +1,6 @@
 package logic;
 
-public class Order {
+public class Order implements Comparable<Order> {
 
 	private int id;
 	private int baseCity;
@@ -8,4 +8,53 @@ public class Order {
 	private String name;
 	private int priority;
 	
+
+	@Override
+	public int compareTo(Order o) {	
+		return priority - o.priority;
+	}
+
+	public int getPriority(){
+		return priority;
+	}
+	
+	public void setPriority(int pr){
+		priority = pr;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getBaseCity() {
+		return baseCity;
+	}
+
+	public void setBaseCity(int baseCity) {
+		this.baseCity = baseCity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getOrderCity() {
+		return orderCity;
+	}
+
+	public void setOrderCity(int orderCity) {
+		this.orderCity = orderCity;
+	}
+	
+	
 }
+
+
